c8645b3ed031c5fb437e92fc5fc721863b8c7d4f
Niklikescode <niklikescode@gmail.com>
2024-01-24 16:49:44 +0000

    server.js set up

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 require('dotenv').config();
+//import express package and assign it to the express variable. This is the package that will help us create the server and handle requests and responses to and from the server and client.
 const express = require('express');
 
-//express app
+//express app instance. This is the server that will handle requests and responses to and from the client. We can use this to listen for requests and send responses.
 const app = express();
 
-//react to requests
+//middleware. This is a function that runs when a request is made to the server. We can use this to log the request method and path to the console. We can also use this to send a response to the client. The next() function is used to tell express to move on to the next middleware function. If we don't use next(), the request will hang and the client will not receive a response.
+app.use((req, res, next) => {
+  console.log(req.method, req.path)
+  next();
+});
+
+//react to requests to the root URL. We can use this to send a response to the client. In this case, we are sending a JSON object with a message property. This is the response that the client will receive.
 app.get('/' , (req , res) => {
     res.json({message: "Welcome to the app"})
 });
